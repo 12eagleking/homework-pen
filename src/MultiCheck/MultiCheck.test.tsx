656915be8a54fd783593f25426a7ea7b91eeb332
niepeng230 <niepeng230@pingan.com.cn>
2020-11-22 06:12:43 +0800

    test: unit test for multi-check selectAll operation

--- src/MultiCheck/MultiCheck.test.tsx
+++ src/MultiCheck/MultiCheck.test.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { render, cleanup, screen, waitFor } from '@testing-library/react';
+import { render, cleanup, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
-import MultiCheck from './MultiCheck';
+import MultiCheck, { Option } from './MultiCheck';
 
 // TODO more tests
 
 describe('MultiCheck', () => {
   const testLabel = 'test label';
+  const options = [
+    {label: 'aaa', value: '111',},
+    {label: 'bbb', value: '222',},
+    {label: 'ccc', value: '333',},
+    {label: 'ddd', value: '444',},
+    {label: 'eee', value: '555',},
+    {label: 'fff', value: '666',},
+    {label: 'ggg', value: '777',},
+    {label: 'hhh', value: '888',},
+    {label: 'iii', value: '999',},
+  ];
 
   describe('initialize', () => {
     afterEach(cleanup);
@@ -19,18 +30,6 @@ describe('MultiCheck', () => {
   });
 
   describe('check list render', () => {
-    const options = [
-      {label: 'aaa', value: '111',},
-      {label: 'bbb', value: '222',},
-      {label: 'ccc', value: '333',},
-      {label: 'ddd', value: '444',},
-      {label: 'eee', value: '555',},
-      {label: 'fff', value: '666',},
-      {label: 'ggg', value: '777',},
-      {label: 'hhh', value: '888',},
-      {label: 'iii', value: '999',},
-    ]
-
     afterEach(cleanup);
 
     it('render an empty check options list if options is empty', () => {
@@ -46,5 +45,43 @@ describe('MultiCheck', () => {
       expect(checkOptions[0]).toHaveTextContent('Select All'); // `Select All` option rendered
       expect(checkOptions.length).toBe(1 + options.length);
     });
+  });
+
+  describe('check list checking operations', () => {
+    afterEach(cleanup);
+
+    it('`Select All` check test', () => {
+      const { container } = render(
+        <MultiCheck label={testLabel} 
+          options={options}
+        />
+      );
+
+      const  checkOptions = container.querySelectorAll(`input[type="checkbox"]`);
+      const selectAllEl = checkOptions[0];
+
+      // initial check list options status -- all options unchecked
+      checkOptions.forEach(el => {
+        expect(el).not.toBeChecked();
+      });
+
+      // `Select All` checked, all other options are checked
+      fireEvent.click(selectAllEl);
+      checkOptions.forEach(el => {
+        expect(el).toBeChecked();
+      });
+
+      // `Select All` unchecked, all other options are unchecked
+      fireEvent.click(selectAllEl);
+      checkOptions.forEach(el => {
+        expect(el).not.toBeChecked();
+      });
+    });
+
+    
+
+    it('if all other options are checked, `Select All` should be checked', () => {});
+
+    it('if any other option are unchecked, `Select All` should be unchecked', () => {});
   });
 });
