34e883d08117d0094b8945c5174728c039d6d4df
niepeng230 <niepeng230@pingan.com.cn>
2020-11-22 20:42:34 +0800

    test: unit test for customized hooks -- useOptionsCheck

---
+++ src/hooks/useOptionsCheck.test.tsx
@@ -0,0 +1,48 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+
+import useOptionsCheck from './useOptionsCheck';
+// import { OptionListProps } from '../MultiCheck/OptionList';
+
+describe('useOptionsCheck', () => {
+  it('initialization, toggle select all option, toggle other check options', () => {
+    const props = {
+      options: [
+        {label: 'aaa', value: '1',},
+        {label: 'bbb', value: '2',},
+      ],
+    };
+    const { result } = renderHook(() => useOptionsCheck(props));
+    expect(result.current.selectAllCheckStatus).toBe(false);
+    expect(result.current.otherCheckedValues).toEqual([]);
+
+    act(() => {
+      result.current.handleSelectAllChange({ checked: true, value: 'all' });
+    });
+    expect(result.current.selectAllCheckStatus).toBe(true);
+    expect(result.current.otherCheckedValues).toEqual(['1', '2']);
+
+    act(() => {
+      result.current.handleSelectAllChange({ checked: false, value: 'all' });
+    });
+    expect(result.current.selectAllCheckStatus).toBe(false);
+    expect(result.current.otherCheckedValues).toEqual([]);
+
+    act(() => {
+      result.current.handleOtherChange({ checked: true, value: '1' });
+    });
+    expect(result.current.selectAllCheckStatus).toBe(false);
+    expect(result.current.otherCheckedValues).toEqual(['1']);
+
+    act(() => {
+      result.current.handleOtherChange({ checked: true, value: '2' });
+    });
+    expect(result.current.selectAllCheckStatus).toBe(true);
+    expect(result.current.otherCheckedValues).toEqual(['1', '2']);
+
+    act(() => {
+      result.current.handleOtherChange({ checked: false, value: '1' });
+    });
+    expect(result.current.selectAllCheckStatus).toBe(false);
+    expect(result.current.otherCheckedValues).toEqual(['2']);
+  });
+});
